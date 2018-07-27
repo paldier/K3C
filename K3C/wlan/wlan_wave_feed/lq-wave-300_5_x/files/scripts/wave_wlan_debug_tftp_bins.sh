2119fe8376a1afc35848224fc6bb4f00916f3dc1
paldier <paldier@hotmail.com>
2018-07-27 12:43:03 +0800

    remove branch 711

--- K3C/wlan/wlan_wave_feed/lq-wave-300_5_x/files/scripts/wave_wlan_debug_tftp_bins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-script_name="wave_wlan_debug_tftp_bins.sh"
-
-[ ! "$LIB_COMMON_SOURCED" ] && . /tmp/wave_wlan_lib_common.sh
-[ ! "$RC_CONF_SOURCED" ] && rc_conf_source
-
-(. $ETC_PATH/wave_wlan_debug.sh tftp_bins)
