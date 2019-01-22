b629574c39619cbfe7c50725ba90fc13afd0bcef
paldier <paldier@hotmail.com>
2019-01-22 20:07:11 +0800

    update

---
+++ K3C/wav500-images/files/wifi_start-bluecave.sh
@@ -0,0 +1,35 @@
+#!/bin/sh /etc/rc.common
+
+# Copyright (c) 2018 paldier <paldier@hotmail.com>
+
+START=21
+
+start()
+{
+
+#--------- init wifi driver   -----------
+if [ ! -n "`lsmod | grep directconnect_datapath`" ]
+then
+insmod /lib/modules/3.10.104/directconnect_datapath.ko
+insmod /lib/modules/3.10.104/dc_mode0-xrx500.ko
+fi
+
+cd /tmp
+cp -s /opt/lantiq/lib/modules/3.10.104/net/mtlkroot.ko /tmp/
+cp -s /lib/firmware/fw_scd_file.scd /tmp/
+cp -s /lib/firmware/hw_scd_file.scd /tmp/
+cp -s /opt/lantiq/lib/modules/3.10.104/net/mtlk.ko /tmp/
+
+echo /opt/lantiq/sbin/hotplug > /proc/sys/kernel/hotplug
+
+export COUNTRY=00
+crda
+mkdir /tmp/wlan_wave
+touch /tmp/wlan_wave/crda_executed
+insmod mtlkroot.ko cdebug=0 rdebug=0
+cp -s /opt/lantiq/bin/logserver /tmp/
+/tmp/logserver -f /tmp/dev/mtlkroot0 -s /tmp/fw_scd_file.scd &
+
+insmod mtlk.ko ap=1,1 fastpath=1,1 ahb_off=1
+}
+
